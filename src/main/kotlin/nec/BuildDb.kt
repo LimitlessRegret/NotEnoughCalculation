87102d3b86c1865c0dd2887fe81041ce09b60ce3
LimitlessRegret <55678532+LimitlessRegret@users.noreply.github.com>
2021-06-11 16:16:33 +1000

    Clean and migrate DB before building it

--- src/main/kotlin/nec/BuildDb.kt
+++ src/main/kotlin/nec/BuildDb.kt
@@ -1,12 +1,21 @@
 package nec
 
 import nec.model.SchemaImporter
+import org.flywaydb.core.Flyway
 
 object BuildDb {
     private val recipeDump = SchemaImporter()
 
     @JvmStatic
     fun main(args: Array<String>) {
+        Flyway.configure().dataSource(
+            "jdbc:sqlite:nec.db",
+            "", ""
+        ).load().apply {
+            clean()
+            migrate()
+        }
+
         recipeDump.loadFile(args[0])
         timeThis("saveToDb") { recipeDump.saveToDb() }
     }
